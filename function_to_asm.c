eee2bd184ab6088fb4aa3dc7bf49cc2ca2d11f94
SadWork <itssadwork@gmail.com>
2024-05-06 16:52:04 +0300

    Добавлено считывание пути входного файла из аргументов командной строки

--- function_to_asm.c
+++ function_to_asm.c
@@ -1,3 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void) { return 0; }
+int main(int argc, char *argv[])
+{
+    char *output_file = NULL;
+
+    // Проверка аргументов командной строки
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-i") == 0 || strcmp(argv[i], "--input") == 0)
+        {
+            // Следующий аргумент после флага должен быть путем к файлу
+            if (i + 1 < argc)
+            {
+                output_file = argv[i + 1];
+            }
+            else
+            {
+                printf("Ошибка: отсутствует путь к входному файлу после флага -i/--input\n");
+                return 1;
+            }
+        }
+    }
+
+    return 0;
+}
