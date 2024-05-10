98bcbb96e9123dfa40a9ea1fdaeebbf43e5deea8
SadWork <itssadwork@gmail.com>
2024-05-10 14:25:45 +0300

    Добавил список констант, флаг DEBUG для отладки программы, начал реализовывать чтение из файла

--- function_to_asm.c
+++ function_to_asm.c
@@ -1,9 +1,28 @@
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
 
+#define EPS      1e-3
+#define BUF_SIZE 2000
+#define DEBUG
+
+typedef struct
+{
+    const char *name;
+    double value;
+} Constant;
+
+Constant constants[] = {
+    { "e",  M_E},
+    {"pi", M_PI}
+};
+
+const int num_constants = sizeof(constants) / sizeof(constants[0]);
+
 int main(int argc, char *argv[])
 {
-    char *output_file = NULL;
+#ifndef DEBUG
+    char *input_file = NULL;
 
     // Проверка аргументов командной строки
     for (int i = 1; i < argc; i++)
@@ -13,7 +32,7 @@ int main(int argc, char *argv[])
             // Следующий аргумент после флага должен быть путем к файлу
             if (i + 1 < argc)
             {
-                output_file = argv[i + 1];
+                input_file = argv[i + 1];
             }
             else
             {
@@ -22,19 +41,28 @@ int main(int argc, char *argv[])
             }
         }
     }
-    if (output_file == NULL)
+    if (input_file == NULL)
     {
         printf("Ошибка: отсутствует путь к входному файлу\n");
         return 1;
     }
+#else
+    const char *input_file = ".debug/spec.txt";
+#endif
 
-    FILE *file = fopen(output_file, "r");
+    FILE *file = fopen(input_file, "r");
 
     if (file == NULL)
     {
-        printf("Не удалось открыть файл %s\n", output_file);
+        printf("Не удалось открыть файл %s\n", input_file);
         return 1;
     }
+
+    double l, r;
+    fscanf(file, "%lf%lf", &l, &r);
+
+    char s[BUF_SIZE];
+    while (fgets(s, BUF_SIZE, file) != NULL) {}
 
     fclose(file);
 
