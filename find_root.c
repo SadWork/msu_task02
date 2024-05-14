8c8c64f8ae4676178e494818554dae66b60cd02d
SadWork <itssadwork@gmail.com>
2024-05-14 15:50:30 +0300

    Добавил комбинированный метод и исправил неточности прошлых версий

--- find_root.c
+++ find_root.c
@@ -3,22 +3,23 @@
 
 #include <math.h>
 
-#define EPS 1e-6
-#define SECANT_METHOD
+#define EPS      1e-6
+#define MAX_ITER 1000
+#define COMBINED_METHOD
 
 #ifdef BISECTION_METHOD
-void find_roots(cvector *x, double l, double r, double (*func)(double))
+double find_root(double l, double r, double (*func)(double))
 {
-    double cur_l = l + EPS, cur_r = r - EPS;
-    if (cur_r - cur_l < EPS)
+    l += EPS, r -= EPS;
+    if (r - l < EPS)
     {
-        return;
+        return NAN;
     }
 
-    double yl = func(cur_l), yr = func(cur_r);
+    double yl = func(l), yr = func(r);
     if (!(signbit(yl) ^ signbit(r)))
     {
-        return; // Одинаковые знаки на концах отрезка
+        return NAN; // Одинаковые знаки на концах отрезка
     }
 
     if (yl > yr)
@@ -30,35 +31,33 @@ void find_roots(cvector *x, double l, double r, double (*func)(double))
     }
 
     double m;
-    while (cur_r - cur_l > EPS)
+    while (r - l > EPS)
     {
-        m = (cur_l + cur_r) / 2;
+        m = (l + r) / 2;
         if (func(m) > 0)
         {
-            cur_r = m;
+            r = m;
         }
         else
         {
-            cur_l = m;
+            l = m;
         }
     }
-    find_roots(x, l + EPS, m - EPS, func);
-    cvector_push_back(x, &m);
-    find_roots(x, m + EPS, r - EPS, func);
+    return m;
 }
 #elif defined(SECANT_METHOD)
-void find_roots(cvector *x, double l, double r, double (*func)(double))
+double find_root(double l, double r, double (*func)(double))
 {
-    double cur_l = l + EPS, cur_r = r - EPS;
-    if (cur_r - cur_l < EPS)
+    l += EPS, r -= EPS;
+    if (r - l < EPS)
     {
-        return;
+        return NAN;
     }
 
-    double yl = func(cur_l), yr = func(cur_r);
+    double yl = func(l), yr = func(r);
     if (!(signbit(yl) ^ signbit(yr)))
     {
-        return; // Одинаковые знаки на концах отрезка
+        return NAN; // Одинаковые знаки на концах отрезка
     }
 
     if (yl > yr)
@@ -67,11 +66,10 @@ void find_roots(cvector *x, double l, double r, double (*func)(double))
         yr         = yl;
         yl         = tmp;
     }
-
     double m;
-    while (fabs(cur_r - cur_l) > EPS)
+    while (fabs(r - l) > EPS)
     {
-        m         = cur_r - (yr * (cur_r - cur_l) / (yr - yl));
+        m         = r - (yr * (r - l) / (yr - yl));
         double ym = func(m);
 
         if (fabs(ym) < EPS)
@@ -81,31 +79,106 @@ void find_roots(cvector *x, double l, double r, double (*func)(double))
 
         if (signbit(ym) == signbit(yl))
         {
-            cur_l = m;
-            yl    = ym;
+            l  = m;
+            yl = ym;
         }
         else
         {
-            cur_r = m;
-            yr    = ym;
+            r  = m;
+            yr = ym;
         }
     }
 
-    find_roots(x, l + EPS, m - EPS, func);
-    cvector_push_back(x, &m);
-    find_roots(x, m + EPS, r - EPS, func);
+    return m;
 }
 #elif defined(NEWTON_METHOD)
+double func_deriv(double x, double (*func)(double)) // error = O(eps^3)
+{
+    double f2 = func(x + EPS), f3 = func(x - EPS);
+    double first_der = (f2 - f3) / (2 * EPS);
+    return first_der;
+}
+double find_root(double l, double r, double (*func)(double))
+{
+    l += EPS, r -= EPS;
+    if (r - l < EPS)
+    {
+        return NAN;
+    }
+    double cur_x = r; // Начальное приближение — середина интервала
 
+    double y = func(cur_x);
+    while (fabs(y) > EPS)
+    {
+        double y_prime = func_deriv(cur_x, func);
+        if (fabs(y_prime) < EPS)
+        {
+            return NAN;
+        }
+        double next_x = cur_x - y / y_prime;
+
+        cur_x = next_x;
+        y     = func(cur_x);
+    }
+    return cur_x;
+}
 #else // COMBINDE_METHOD
+double func_second_derivative(double x, double (*func)(double)) // error = O(eps^4)
+{
+    double f1 = func(x), f2 = func(x + EPS), f3 = func(x - EPS);
+    double second_der = (f2 + f3 - 2 * f1) / EPS / EPS;
+    return second_der;
+}
+double func_deriv(double x, double (*func)(double)) // error = O(eps^3)
+{
+    double f2 = func(x + EPS), f3 = func(x - EPS);
+    double first_der = (f2 - f3) / (2 * EPS);
+    return first_der;
+}
+
+double find_root(double l, double r, double (*func)(double))
+{
+    l += EPS, r -= EPS;
+    if (r - l < EPS)
+    {
+        return NAN;
+    }
+
+    double y = func((r + l) / 2);
+    while (fabs(y) > EPS)
+    {
+        double yl = func(l), yr = func(r);
+
+        double y_prime        = func_deriv((l + r) / 2, func);
+        double y_double_prime = func_second_derivative((l + r) / 2, func);
+
+        if (fabs(y_prime) < EPS)
+        {
+            return NAN;
+        }
+        else if (signbit(y_prime) == signbit(y_double_prime))
+        {
+            y_prime         = func_deriv(r, func);
+            double m_secant = l - (yl * (r - l) / (yr - yl));
+            double m_newton = r - yr / y_prime;
+            l = m_secant, r = m_newton;
+        }
+        else
+        {
+            y_prime         = func_deriv(l, func);
+            double m_secant = r - (yr * (r - l) / (yr - yl));
+            double m_newton = l - yl / y_prime;
+            r = m_secant, l = m_newton;
+        }
+        y = func((l + r) / 2);
+    }
+    return (l + r) / 2;
+}
 
 #endif
-double f1(double x) { return x + 2; };
+double f1(double x) { return x * x - 2; };
 int main()
 {
-    cvector x_vec;
-    double *x = cvector_init(&x_vec, sizeof(double));
-    find_roots(&x_vec, -4, 4, f1);
-    x = cvector_free(&x_vec);
+    printf("%.12lf\n", find_root(-4, 4, f1));
     return 0;
 }
