c393181a27a646206b72e7a1122ff1000046840c
SadWork <itssadwork@gmail.com>
2024-05-15 19:59:21 +0300

    Добавил заголовочный файл для root.c и файл хранящий отрезок интегрирования

---
+++ root.h
@@ -0,0 +1,4 @@
+#ifndef ROOT_H
+#define ROOT_H
+double find_root(double l, double r, double (*func)(double), int *itrs);
+#endif
---
+++ segment.h
@@ -0,0 +1,2 @@
+const double LEFT_EDGE = 0.000000;
+const double RIGHT_EDGE = 4.000000;
